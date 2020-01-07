093f998e233322a0f183775b7728012601e1eca6
Gregory Davis <gregory.davis@autozone.com>
2020-01-06 22:21:08 -0600

    Added home component and search component as well as side nav

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import React from 'react'
-import styled from 'styled-components/native'
-
-const Container = styled.View`
-  align-items: center;
-  background-color: black;
-  flex: 1;
-  justify-content: center;
-`
-
-const Text = styled.Text`
-  color: white;
-`
+import AppNavigator from './Components/AppNavigator'
 
 export default function App() {
   return (
-    <Container>
-      <Text>Open up App.js to start working on your app!</Text>
-    </Container>
+    <AppNavigator />
   )
 }
